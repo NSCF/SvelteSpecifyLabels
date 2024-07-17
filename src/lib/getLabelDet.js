3407f70493034c2120d295b77370529754aa53d6
Ian Engelbrecht <ianicus.za@gmail.com>
2024-07-17 20:30:38 +0200

    fixed adding scientificName to labelDet

--- src/lib/getLabelDet.js
+++ src/lib/getLabelDet.js
@@ -1,6 +1,6 @@
 
 
-export default function(labelRecord, includeTaxonAuthorities) { 
+export default function(labelRecord, includeAuthority, addItalics) { 
   //TODO add sensu and other suffixes when we have it
 
   let labelDet
@@ -8,64 +8,69 @@ export default function(labelRecord, includeTaxonAuthorities) {
   if(labelRecord.verbatimIdentification){
     labelDet = labelRecord.verbatimIdentification
     if(!labelDet.includes(' sp.') && !labelDet.includes('?')) {
-      if(includeTaxonAuthorities && labelRecord.verbatimIdentification == labelRecord.canonicalName && labelRecord.scientificNameAuthorship) {
+      if(includeAuthority && labelRecord.verbatimIdentification == labelRecord.canonicalName && labelRecord.scientificNameAuthorship) {
         labelDet += ' ' + labelRecord.scientificNameAuthorship
       }
     }
   }
-  else {
-    if(labelRecord.canonicalName && labelRecord.canonicalName.trim()){
+  else if(labelRecord.canonicalName && labelRecord.canonicalName.trim()){
     
-      let questionMark = false
-      if(labelRecord.identificationConfidence){
-        if(labelRecord.identificationConfidence.toLowerCase() != 'high'){
-          questionMark = true
-        }
-      }
-
-      if(labelRecord.identificationQualifier){
-        if(['aff.', 'cf.', 'nr'].includes(labelRecord.identificationQualifier)){
-          let nameParts = labelRecord.canonicalName.split(' ')
-          if(nameParts.length > 1){
-            let lastPart = nameParts.pop()
-            nameParts.push(labelRecord.identificationQualifier)
-            nameParts.push(lastPart)
-            if(questionMark){
-              nameParts.push('?')
-            }
-            else if(labelRecord.scientificNameAuthorship && includeTaxonAuthorities && !nameParts.includes('sp.')){
-              nameParts.push(labelRecord.scientificNameAuthorship)
-            }
-            labelDet = nameParts.join(' ')
-          }
-          else {
-            labelDet = [labelRecord.identificationQualifier, labelRecord.canonicalName].join(' ')
-            if(questionMark){
-              labelDet += ' ?'
-            }
-            else if (labelRecord.scientificNameAuthorship && includeTaxonAuthorities  && !labelDet.includes(' sp.')){
-              labelDet += ` ${labelRecord.scientificNameAuthorship}`
-            }
-          }
-        }
-        else {
-          labelDet = [labelRecord.canonicalName, labelRecord.identificationQualifier].join(' ')
-        } 
-      }
-      else {
-        labelDet = labelRecord.canonicalName
-        if(questionMark) {
-          labelDet += ' ?'
-        }
-        else if (labelRecord.scientificNameAuthorship && includeTaxonAuthorities && !labelDet.includes(' sp.')){
-          labelDet += ` ${labelRecord.scientificNameAuthorship}`
-        }
+    
+    let questionMark = false
+    if(labelRecord.identificationConfidence){
+      if(labelRecord.identificationConfidence.toLowerCase() != 'high'){
+        questionMark = true
       }
     }
+
+    if(labelRecord.identificationQualifier){
+      if(['aff.', 'cf.', 'nr'].includes(labelRecord.identificationQualifier)){
+        let nameParts = labelRecord.canonicalName.split(' ').filter(x => x.trim()).map(x => x.trim())
+        if (addItalics) {
+
+        }
+        if(nameParts.length > 1){
+          let lastPart = nameParts.pop()
+          nameParts.push(labelRecord.identificationQualifier)
+          nameParts.push(lastPart)
+          if(questionMark){
+            nameParts.push('?')
+          }
+          else if(labelRecord.scientificNameAuthorship && includeAuthority && !nameParts.includes('sp.')){
+            nameParts.push(labelRecord.scientificNameAuthorship)
+          }
+          labelDet = nameParts.join(' ')
+        }
+        else {
+          labelDet = [labelRecord.identificationQualifier, labelRecord.canonicalName].join(' ')
+          if(questionMark){
+            labelDet += ' ?'
+          }
+          else if (labelRecord.scientificNameAuthorship && includeAuthority  && !labelDet.includes(' sp.')){
+            labelDet += ` ${labelRecord.scientificNameAuthorship}`
+          }
+        }
+      }
+      else {
+        labelDet = [labelRecord.canonicalName, labelRecord.identificationQualifier].join(' ')
+      } 
+    }
     else {
-
-      labelDet = labelRecord.verbatimIdentification
-
+      labelDet = labelRecord.canonicalName
+      if(questionMark) {
+        labelDet += ' ?'
+      }
+      else if (labelRecord.scientificNameAuthorship && includeAuthority && !labelDet.includes(' sp.')){
+        labelDet += ` ${labelRecord.scientificNameAuthorship}`
+      }
+    }
+  }
+  else if (labelRecord.scientificName) {
+    labelDet = labelRecord.scientificName
+    if (includeAuthority) {
+      if (labelRecord.scientificNameAuthorship && !labelRecord.scientificName.includes(labelRecord.scientificNameAuthorship)){
+        labelDet += ' ' + labelRecord.scientificNameAuthorship
+      }
     }
   }
 
