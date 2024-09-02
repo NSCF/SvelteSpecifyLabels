b451a0e388f1315274b7aa2f6a96a8918276d232
Hester <h.steyn@sanbi.org.za>
2024-09-02 16:10:10 +0200

    veranderinge in Afrikaanse teks van vorige commits

--- src/components/langs/Afr.svelte
+++ src/components/langs/Afr.svelte
@@ -12,12 +12,12 @@
 <h3>Laai jou data</h3>
 <p>
   Begin deur 'n CSV-lêer met jou data op die tuisblad te laai. <a href="https://en.wikipedia.org/wiki/Comma-separated_values" target="_blank">CSV</a>
-  is die eenvoudigste en maklikste formaat vir die uitruil van data. Hier is 'n <a href={exampleDataURL} target="_blank">voorbeeld</a> wat jy kan 
+  is die eenvoudigste en veiligste formaat vir die uitruil van data. Hier is 'n <a href={exampleDataURL} target="_blank">voorbeeld</a> wat jy kan 
   gebruik indien jy nie reeds een het nie.
 </p>
 <p>
-  As jy jou datastel laai en geen etikette sien nie, gaan na veldpassings en verfris die passings. Dit behoort jou ten minste 'n etiket te gee 
-  om mee te begin. Jy kan dan veranderings maak aan veldpassings soos nodig (sien Datavelde hieronder).
+  Indien jy jou datastel laai en geen etikette sien nie, gaan na veldpassings en verfris die passings. Dit behoort jou ten minste 'n etiket te gee 
+  om mee te begin. Jy kan dan veranderings maak aan die veldpassings soos nodig (sien Datavelde hieronder).
 </p>
 <p>
   <em>
@@ -30,9 +30,9 @@
 <h3>Datavelde</h3>
 <p>
   Hierdie hulpmiddel gebruik standaard datavelde soos ligging, datum, taksonnaam, ens. en maak gebruik van 
-  <a href="https://dwc.tdwg.org/terms/" target="_blank">Darwin Core-velde</a> so ver as moontlik. Wanneer jy jou data laai, probeer die hulpmiddel om 
-  die velde in jou datastel met die ooreenstemmende etiketvelde te verbind. As jy byvoorbeeld 'n veld genaamd 'localityname' 
-  het, sal die data geskryf word na die veld 'locality'. Die idee is dat jy jou data kan intrek met watter velde jy 
+  <a href="https://dwc.tdwg.org/terms/" target="_blank">Darwin Core-velde</a> so ver as moontlik. Wanneer jy jou data laai, 
+  probeer die hulpmiddel om die velde in jou datastel met die ooreenstemmende etiketvelde te verbind. As jy byvoorbeeld 'n veld 
+  genaamd 'localityname' het, sal die data geskryf word na die veld 'locality'. Die idee is dat jy jou data kan intrek met watter velde jy 
   ook al het en die hulpmiddel die vergelyking laat doen, sodat jy nie ekstra werk hoef te doen nie.
 </p>
 <p>
@@ -42,7 +42,7 @@
 </p>
 <p>
   Onthou dat die velde wat jy op die etiket insluit, jou keuse is. Die hulpmiddel benodig ten minste een veld, soos 
-  ligging of taksonnaam, om 'n etiket te genereer, maar andersins is jy vry om te kies en keur soos jy wil. 
+  ligging of taksonnaam om 'n etiket te genereer, maar andersins is jy vry om te kies en keur soos jy wil. 
 </p>
 <p>
   Vir herbarium-etikette kan jy ook velde soos 'dups' insluit om instansies te lys waarheen duplikate gestuur word, 
@@ -56,8 +56,8 @@
 
 <h5>Taksonnaamvelde</h5>
 <p>
-  Taksonname kan op baie verskillende maniere gestoor word. Die hulpmiddel gee prioriteit aan name wat in individuele 
-  rangvelde gestoor word, dit wil sê familie, genus, spesie, ens. (tot subforma vir plante). Alternatiewelik kan jy 
+  Taksonname kan op baie verskillende maniere in 'n datastel gestoor word. Die hulpmiddel gee prioriteit aan name wat in individuele 
+  rangvelde gestoor word, dit wil sê familie, genus, spesie, ens. (tot subforma vir plante). Andersins kan jy 
   een veld vir die taksonnaam gebruik en die naam daarin stoor ongeag sy rang ('n aparte veld, taksonrang, kan ingesluit word). 
   Dit word aanbeveel om aparte velde vir die outeursitaat, spesifikasies, akkuraatheid, ens. te gebruik. Jy kan ook enige 
   opmerkings rakende die identifikasie, literatuurbronne, ens. insluit.
@@ -67,7 +67,7 @@
 <p>
   Die <a href={exampleDataURL} target="_blank">voorbeelddata</a> is a Google Sheet wat velde insluit 
   wat deur die hulpmiddel herken sal word, asook 'n paar voorbeelde van rekords. Neem 'n paar oomblikke om deur die 
-  veldopskrifte te gaan en lees die opmerkings wat beskryf hoe elke veld gebruik moet word. Jy is welkom om 'n kopie
+  kommentaar in die veldopskrifte te gaan wat verduidelik hoe elke veld gebruik moet word. Jy is welkom om 'n kopié
   van hierdie bladsy vir eie gebruik te maak (File >> Make a copy).
 <p>
   Om etikette van 'n Google Sheet te maak, moet jy die data eers in CSV-formaat aflaai. Gaan na File >> Download, 
@@ -76,37 +76,38 @@
 <p>
   Die sigblad bevat 'n veld genaamd 'print', wat gebruik kan word vir die kies en filter van rekords om te druk.  
   <a href="https://youtu.be/SYSevA4znlc?si=oO6e0d_pxyUikpEd" target="_blank"> Hierdie 3 minuut video</a> wys hoe om in Google Sheets
-  te filter. Om etikette te druk vir gefilterde rekords, moet jy die rekords kopieer en in 'n aparte sigblad plak 
+  te filter. Om slegs etikette vir gefilterde rekords te druk, moet jy die rekords kopieer en in 'n aparte sigblad plak 
   voordat jy dit aflaai. Ongelukkig ignoreer Google Sheets enige toegepaste filters en alle rekords 
-  in 'n sigblad word uitgevoer. Jy kan soveel verskillende filters gebruik soos jy wil. Jy wil dalk velde insluit 
-  vir die instituut waar die eksemplare gestoor word en die datum waarop eksemplare gestoor word, om sodoende 
+  in 'n sigblad word uitgevoer. Jy kan soveel verskillende filters gebruik soos jy wil, byvoorbeeld velde insluit 
+  vir die instituut en die datum waarop eksemplare gestoor word, om sodoende 
   etikette te druk vir groepe eksemplare soos dit in die versameling opgeneem word.
 </p>
 
 <h3>Duplikaat etikette</h3>
 <p>
   As jy duplikaat etikette vir elke eksemplaar in 'n reeks wil hê (bv. verskeie insekte van dieselfde spesie wat saam versamel is, 
-  of duplikate van herbarium eksemplare), sal jy 'n veld by jou datastel moet voeg met 'n aantal (noem die veld 'count'). 
-  Indien jy nie 'n spesifieke aantalveld het nie, sal die instrument die veld met aantal eksemplare per ouderdom/geslag gebruik. 
+  of duplikate van herbarium eksemplare), sal jy 'n veld by jou datastel moet voeg met 'n aantal (noem die veld 'count' of 'specimenCount'). 
+  Indien jy nie 'n spesifieke aantalveld het nie, sal die instrument die veld met aantal eksemplare per ouderdom/geslag  of 
+  'dups' (herbarium-etikette) gebruik. 
 </p>
 
 <h3>Etiket opsies</h3>
 <p>
-  Sodra jy jou datastel ingetrek het, sal jy 'n lys opsies sien om jou etiketontwerp aan te pas. Wanneer jy op drukknoppie kliek, 
+  Sodra jy jou datastel ingetrek het, sal jy 'n lys opsies sien om jou etiketontwerp aan te pas. Wanneer jy op die drukknoppie kliek, 
   sal jou keuses (en veldskakels) gestoor word vir die volgende keer wanneer jy etikette wil druk. Gebruik 'n klein 
   datastel om die etiketontwerp baas te raak, voordat jy groot groepe etikette maak.
 </p>
 <p>
   <em>
-    Dit word sterk aanbeveel dat jy 'n <strong>klein aantal toetsetikette</strong> druk om seker te maak dat jou ontwerp goed werk, 
+    Dit word sterk aanbeveel dat jy 'n <strong>klein aantal toetsetikette</strong> druk om seker te maak dat jou ontwerp goed werk op papier, 
     voordat jy groot groepe finale etikette druk. 
   </em>
 </p>
 
 <h3>Druk van etikette</h3>
 <p>
-  Wanneer jy op die drukknoppie kliek, sal die blaaier die standaard drukdialoog oopmaak. Maak seker dat jy toepaslike bladsygrootte 
-  en kantlyne kies voordat jy druk. Jy kan of direk druk of die etikette as 'n PDF stoor en later druk. Dit word aanbeveel dat die 
+  Wanneer jy op die drukknoppie op die voorskoublad (preview screen) kliek, sal die blaaier die standaard drukdialoog oopmaak. Maak seker dat jy toepaslike bladsygrootte 
+  en kantlyne kies voordat jy druk. Jy kan of direk druk of die etikette as 'n PDF stoor en later kontroleer en druk. Dit word aanbeveel dat die 
   'Save as PDF'-opsie eerder as die 'Microsoft Print to PDF' gebruik word, aangesien dit beter resultate gee. 
 </p>
 <p>
